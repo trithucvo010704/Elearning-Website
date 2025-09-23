94ba6cf5f5e73a6c7940962eb1d1fc1bacfc879a
vo tri thuc <trithucvo12345@gmail.com>
2025-09-23 16:34:14 +0700

    Create Base Project

---
+++ src/main/java/com/example/elearning_api/Enum/Role.java
@@ -0,0 +1,4 @@
+package com.example.elearning_api.Enum;
+
+public class Role {
+}
---
+++ src/test/java/com/example/elearning_api/ElearningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elearning_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearningApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
