3d26ae41186d11e5888380bfcc5fa1ff8ff50324
vo tri thuc <trithucvo12345@gmail.com>
2025-10-07 19:41:45 +0700

    feat(week2-auth): finalize authentication module with frontend, AuthUserDetailsService, and EnrollmentStatus enum

---
+++ src/main/java/com/example/elearning_api/service/auth/AuthUserDetailsService.java
@@ -0,0 +1,18 @@
+package com.example.elearning_api.service.auth;
+
+import com.example.elearning_api.repository.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service @RequiredArgsConstructor
+public class AuthUserDetailsService implements UserDetailsService {
+    private final UserRepo users;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return users.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+    }
+}
