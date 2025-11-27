0d0154a16cd3d91348311c04f866b26d1e36aa1f
Dinh-Linh <linhnd26052004@gmail.com>
2025-11-27 22:07:42 +0700

    Add enum

--- src/main/java/com/example/elearning_api/Enum/Provider.java
+++ src/main/java/com/example/elearning_api/Enum/Provider.java
@@ -1,5 +1,6 @@
 package com.example.elearning_api.Enum;
 // Nhà cung cấp thanh toán/webhook (payments.provider, webhook_events.provider)
 public enum Provider {
-    STRIPE             // Stripe
+    STRIPE,             // Stripe
+    VNPAY               //VNPay
 }
